e22b9a680326d5966e3a48377c2051bf4c165c72
Eric Darmusié <formaderic@gmail.com>
2022-05-30 21:42:23 +0200

    netbeans1

---
+++ core/src/main/java/com/myexpoonline/store/core/entity/Work.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.myexpoonline.store.core.entity;
+
+/**
+ *
+ * @author eric
+ */
+public class Work {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Work(int id) {
+        this.id = id;
+    }
+    private String title;
+    private Author author;
+    private int year;
+    private String style;
+    private String description;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setStyle(String style) {
+        this.style = style;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public Work() {
+        
+    }
+    public Work(String title) {
+        this.title = title;
+    }
+}
